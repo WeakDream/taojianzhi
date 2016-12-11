abc668cc16ba652deebd01ccc58341606159b783
sywing <1102977704@qq.com>
2016-12-11 14:31:00 +0800

    taojianzhi

--- resources/views/taojianzhi/personal_center.blade.php
+++ resources/views/taojianzhi/personal_center.blade.php
@@ -102,10 +102,10 @@
             var top=$(".shenghuo").offset().top+400;
             $(".bj").css("height",top);
         });
-        $("#zhanghushezhi").click(function()
-        {
-            location.href="test1";
-        });
+//        $("#zhanghushezhi").click(function()
+//        {
+//            location.href="test1";
+//        });
 
 
         $(".maijia").click(function(){
@@ -1251,8 +1251,8 @@
         <p style="margin-left: 55px;font-size: 16px;margin-top:30px;">简历是求职的利器，填写简历才能尽快找到好工作！</p>
         <p style="margin-left: 55px;font-size: 16px;">去填写一份优质的简历吧，认真的人，才能让认真的企业找上你！</p>
         <p style="margin-left: 55px;font-size:18px;font-weight: 700;">请在这设计你的简历吧：</p>
-
-
+        <p><span id="jianli" style="margin-left: 55px;border: 1px solid #99ffcc;height: 56px;width:186px;display: inline-block;border-radius: 8px;background: #66ccff;"><a href="{{url('test1')}}" style="text-decoration: none;margin-top:10px;margin-left:40px;font-size: 25px;position: absolute">点击去填写</a></span>
+        </p>
 
         <div class="zhaopintan1">
             <h3 style="color: #FF5500;margin-left: 40px;">职位管理</h3>
